754b55cd30b65412702469aa0724a7bf83552830
leejay210 <leejay@ponylink.co.kr>
2025-04-09 16:42:00 +0900

    add approval grid structure

--- src/components/approvalGrid/ApprovalGrid.jsx
+++ src/components/approvalGrid/ApprovalGrid.jsx
@@ -1,11 +1,22 @@
+import './ApprovalGrid.css';
+
 export const ApprovalGrid = () => {
-  return(
-    <>
-      <div className={"approval-grid-container"}>
-        sign~
-      </div>
-    </>
-  )
+  const contents = [
+    "작성자", "A2", "A3", "A4", "A5",
+    "나~", "B2", "B3", "B4", "B5",
+    "C1", "C2", "C3", "C4", "C5",
+    "D1", "D2", "D3", "D4", "D5",
+  ];
+
+  return (
+    <div className="grid-container">
+      {contents.map((text, idx) => (
+        <div key={idx} className="cell">
+          {text}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ApprovalGrid;
