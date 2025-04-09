eb1a6bda3c2b85f748526a3c0cd4b257f5c633b2
leejay210 <leejay@ponylink.co.kr>
2025-04-09 16:44:09 +0900

    approval grid style

--- src/components/approvalGrid/ApprovalGrid.jsx
+++ src/components/approvalGrid/ApprovalGrid.jsx
@@ -2,7 +2,7 @@ import './ApprovalGrid.css';
 
 export const ApprovalGrid = () => {
   const contents = [
-    "작성자", "A2", "A3", "A4", "A5",
+    "작성자", "팀장", "A3", "A4", "A5",
     "나~", "B2", "B3", "B4", "B5",
     "C1", "C2", "C3", "C4", "C5",
     "D1", "D2", "D3", "D4", "D5",
