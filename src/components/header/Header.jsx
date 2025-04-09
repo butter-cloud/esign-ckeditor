181ff80846af7450a64a86cb580510935f6a0023
leejay210 <leejay@ponylink.co.kr>
2025-04-09 16:55:13 +0900

    add header grid style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css';
 import ApprovalGrid from "../approvalGrid/ApprovalGrid.jsx";
+import { Fragment } from "react";
 
 export const Header = () => {
   const labels = ["입안일자", "보존년한", "문서번호", "입안부서", "입 안 자"];
@@ -7,24 +8,24 @@ export const Header = () => {
   return (
     <div className="doc-table">
       {labels.map((label, idx) => (
-        <React.Fragment key={idx}>
+        <Fragment key={idx}>
           <div className="cell label">{label}</div>
-          <div className="cell value">
-            <input type="text" />
-          </div>
-          {idx === 0 && (
-            <div className="cell textarea" rowSpan={5}>
-              <textarea placeholder="내용을 입력하세요" />
-            </div>
-          )}
-        </React.Fragment>
+          <div className="cell value"><input type="text" /></div>
+        </Fragment>
       ))}
-      <div className="cell label">제&nbsp;&nbsp;&nbsp;&nbsp;목</div>
-      <div className="cell value" colSpan={2}>
+
+      {/* 오른쪽 병합 셀: 5행 세로 병합, 3번째 column */}
+      <div className="cell textarea">
+        <textarea placeholder="내용을 입력하세요" />
+      </div>
+
+      {/* 마지막 줄 */}
+      <div className="cell label title-label">제&nbsp;&nbsp;&nbsp;&nbsp;목</div>
+      <div className="cell value title-input">
         <input type="text" defaultValue="테스트" />
       </div>
     </div>
   );
-}
+};
 
 export default Header;
