222d072d5dcc2415ca9a9858dbe2834a9b8262b9
leejay210 <leejay@ponylink.co.kr>
2025-04-09 17:12:05 +0900

    add document title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.css';
 import ApprovalGrid from "../approvalGrid/ApprovalGrid.jsx";
 import { Fragment } from "react";
-import App from "../../App.jsx";
 
 export const Header = () => {
   const labels = ["입안일자", "보존년한", "문서번호", "입안부서", "입 안 자"];
 
   return (
+    <>
+    <div className="doc-title">품 의 서</div>
     <div className="doc-table">
       {labels.map((label, idx) => (
         <Fragment key={idx}>
@@ -26,6 +27,7 @@ export const Header = () => {
         <input type="text" defaultValue="테스트" />
       </div>
     </div>
+    </>
   );
 };
 
