d9b47bd4d004d7dc85bce34fa569f25efbc0040d
leejay <leejay@ponylink.co.kr>
2025-04-10 12:16:51 +0900

    apply header style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,18 +3,30 @@ import ApprovalGrid from "../approvalGrid/ApprovalGrid.jsx";
 import { Fragment } from "react";
 
 export const Header = () => {
-  const labels = ["입안일자", "보존년한", "문서번호", "입안부서", "입 안 자"];
 
   return (
     <>
     <div className="doc-title">품 의 서</div>
     <div className="doc-table">
-      {labels.map((label, idx) => (
-        <Fragment key={idx}>
-          <div className="cell label">{label}</div>
-          <div className="cell value"><input type="text" /></div>
-        </Fragment>
-      ))}
+      {/* 입안일자 */}
+      <div className="cell label">입안일자</div>
+      <div className="cell value"><span>2025-04-01</span></div>
+
+      {/* 보존년한 */}
+      <div className="cell label">보존년한</div>
+      <div className="cell value"><span>5년</span></div>
+
+      {/* 문서번호 */}
+      <div className="cell label">문서번호</div>
+      <div className="cell value"><span></span></div>
+
+      {/* 입안부서 */}
+      <div className="cell label">입안부서</div>
+      <div className="cell value"><span>서비스개발팀</span></div>
+
+      {/* 입안자 */}
+      <div className="cell label">입 안 자</div>
+      <div className="cell value"><span>이승지</span></div>
 
       {/* 오른쪽 병합 셀: 5행 세로 병합, 3번째 column */}
       <div className="cell textarea">
@@ -24,7 +36,7 @@ export const Header = () => {
       {/* 마지막 줄 */}
       <div className="cell label title-label">제&nbsp;&nbsp;&nbsp;&nbsp;목</div>
       <div className="cell value title-input">
-        <input type="text" defaultValue="테스트" />
+        <input type="text" defaultValue="" />
       </div>
     </div>
     </>
